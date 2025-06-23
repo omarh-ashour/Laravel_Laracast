d666c776e7973cb9006256d934e36a0f7bd4b9fc
AsamehELsayed <abdelahman2003.12.12@gmail.com>
2025-06-23 14:01:36 +0300

    resources edited only in this commit

--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,2 +1,5 @@
-
-Congrats! Your job is now live on our website.
+<h1><strong>{{ $job->title }}</strong></h1>
+<h3>Congrats! Your job is now live on our website.</h3>
+<p>
+    <a href="{{ url('/jobs/'.$job->id) }}">View Your Job Listing</a>
+</p>
